162433caa113dc7e2b70c42c9af56c8a80113398
Matias Massetti <matiasmassetti@gmail.com>
2020-07-08 11:20:37 -0300

    added dataloader to improve performance

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,11 +1,20 @@
+const DataLoader = require("dataloader");
 const Item = require("../../models/item");
 const User = require("../../models/user");
 const { dateToString } = require("../../helpers/date");
 
-const items = async itemIds => {
+const itemLoader = new DataLoader((itemIds) => {
+  return items(itemIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+  return User.find({ _id: { $in: userIds } });
+});
+
+const items = async (itemIds) => {
   try {
     const items = await Item.find({ _id: { $in: itemIds } });
-    return items.map(item => {
+    return items.map((item) => {
       return transformItem(item);
     });
   } catch (err) {
@@ -13,45 +22,45 @@ const items = async itemIds => {
   }
 };
 
-const singleItem = async itemId => {
+const singleItem = async (itemId) => {
   try {
-    const item = await Item.findById(itemId);
-    return transformItem(item);
+    const item = await itemLoader.load(itemId.toString());
+    return item;
   } catch (err) {
     throw err;
   }
 };
 
-const user = async userId => {
+const user = async (userId) => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
-      createdItems: items.bind(this, user._doc.createdItems)
+      createdItems: () => itemLoader.loadMany(this, user._doc.createdItems),
     };
   } catch (err) {
     throw err;
   }
 };
 
-const transformItem = item => {
+const transformItem = (item) => {
   return {
     ...item._doc,
     _id: item.id,
     date: dateToString(item._doc.date),
-    creator: user.bind(this, item.creator)
+    creator: user.bind(this, item.creator),
   };
 };
 
-const transformClaim = claim => {
+const transformClaim = (claim) => {
   return {
     ...claim._doc,
     _id: claim.id,
     claimerUser: user.bind(this, claim._doc.claimerUser),
     item: singleItem.bind(this, claim._doc.item),
     createdAt: dateToString(claim._doc.createdAt),
-    updatedAt: dateToString(claim._doc.updatedAt)
+    updatedAt: dateToString(claim._doc.updatedAt),
   };
 };
 
