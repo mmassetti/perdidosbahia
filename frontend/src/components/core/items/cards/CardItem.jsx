8839d79df3c7f88a13355db3aeef521a52301cea
Matias Massetti <matiasmassetti@gmail.com>
2020-08-01 07:26:35 -0300

    show message to user if has a pending claim for an item

--- frontend/src/components/core/items/cards/CardItem.jsx
+++ frontend/src/components/core/items/cards/CardItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Redirect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Badge,
   Card,
@@ -15,6 +15,63 @@ require("moment/locale/es");
 const CardItem = (props) => {
   let history = useHistory();
   const [isLoading, setIsLoading] = useState(false);
+  const [claimAlreadyExists, setClaimAlreadyExists] = useState(false);
+
+  const fetchClaims = () => {
+    setIsLoading(true);
+    const requestBody = {
+      query: `
+          query {
+            claims {
+              _id  
+              itemClaimer {
+                _id
+              }
+              stateForClaimer
+            }
+          }
+        `,
+    };
+
+    fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + props.token,
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        const claims = resData.data.claims;
+
+        if (claims && claims.length > 0) {
+          claims.map((claim, index) => {
+            if (claim.itemClaimer._id == props.authUserId) {
+              setClaimAlreadyExists(true);
+            }
+          });
+        }
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    if (props.token) {
+      fetchClaims();
+    }
+  }, []);
 
   function goToSingleItem() {
     let { onDelete, ...neededProps } = props;
@@ -24,6 +81,81 @@ const CardItem = (props) => {
       state: { props: neededProps },
     });
   }
+
+  function goToUserClaims() {
+    history.push({
+      pathname: "/mis-publicaciones",
+    });
+  }
+
+  const showContent = () => {
+    return (
+      <Col lg="4">
+        <Card
+          className="card-lift--hover shadow border-0"
+          style={{ marginBottom: "1rem" }}
+        >
+          <CardBody className="py-5">
+            {props.creatorId == props.authUserId ? (
+              <h6 className="text-warning font-weight-light mb-2">
+                ¡Esta es tu publicación!
+              </h6>
+            ) : (
+              ""
+            )}
+
+            {claimAlreadyExists ? (
+              <React.Fragment>
+                {" "}
+                <h6 className="text-warning font-weight-light mb-2">
+                  ¡Ya tenés un reclamo en marcha por este objeto!
+                </h6>
+                <Button
+                  className="mb-4 btn-round"
+                  color="primary"
+                  size="sm"
+                  outline
+                  onClick={() => goToUserClaims()}
+                >
+                  Ver reclamo
+                </Button>
+              </React.Fragment>
+            ) : (
+              ""
+            )}
+
+            <h6 className="text-primary text-uppercase">
+              {props.category != "otro" ? props.category : "Otros objetos"}
+            </h6>
+            <p className="description mt-3">{props.description}</p>
+            <div>
+              <Badge color="primary" pill className="mr-1">
+                {props.type}
+              </Badge>
+              <Badge color="primary" pill className="mr-1">
+                {moment(props.date).format("LL")}
+              </Badge>
+            </div>
+            {!claimAlreadyExists ? (
+              <Button
+                className="mt-4 btn-round"
+                color="primary"
+                onClick={() => goToSingleItem()}
+              >
+                Ver más
+              </Button>
+            ) : (
+              ""
+            )}
+
+            {props.token && props.authUserId == props.creatorId
+              ? deleteItemButton()
+              : ""}
+          </CardBody>
+        </Card>
+      </Col>
+    );
+  };
 
   const deleteItemButton = () => {
     return (
@@ -43,47 +175,7 @@ const CardItem = (props) => {
     );
   };
 
-  return (
-    <Col lg="4">
-      <Card
-        className="card-lift--hover shadow border-0"
-        style={{ marginBottom: "1rem" }}
-      >
-        <CardBody className="py-5">
-          {props.creatorId == props.authUserId ? (
-            <h6 className="text-warning font-weight-light mb-2">
-              ¡Esta es tu publicación!
-            </h6>
-          ) : (
-            ""
-          )}
-          <h6 className="text-primary text-uppercase">
-            {props.category != "otro" ? props.category : "Otros objetos"}
-          </h6>
-          <p className="description mt-3">{props.description}</p>
-          <div>
-            <Badge color="primary" pill className="mr-1">
-              {props.type}
-            </Badge>
-            <Badge color="primary" pill className="mr-1">
-              {moment(props.date).format("LL")}
-            </Badge>
-          </div>
-          <Button
-            className="mt-4 btn-round"
-            color="primary"
-            onClick={() => goToSingleItem()}
-          >
-            Ver
-          </Button>
-
-          {props.token && props.authUserId == props.creatorId
-            ? deleteItemButton()
-            : ""}
-        </CardBody>
-      </Card>
-    </Col>
-  );
+  return showContent();
 };
 
 export default CardItem;
