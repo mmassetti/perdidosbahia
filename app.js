8a34ef6c00cbd86477f1cc8377a47d78a01daeef
Matias Massetti <matiasmassetti@gmail.com>
2020-08-05 13:59:10 -0300

    trying to put files outside backend folder

--- app.js
+++ app.js
@@ -30,11 +30,9 @@ app.use(
 );
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("../frontend/build"));
+  app.use(express.static("frontend/build"));
   app.get("/*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../frontend/", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -42,7 +42,7 @@ module.exports = {
 
     const token = jwt.sign(
       { userId: user.id, email: user.email },
-      process.env.SECRET,
+      process.env.SECRET || "somesupersecretkey",
       {
         expiresIn: "1h",
       }
